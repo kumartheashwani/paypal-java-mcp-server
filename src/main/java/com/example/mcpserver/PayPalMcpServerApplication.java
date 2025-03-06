92f5dcae65e95ba46360f401508fd0dccaad3f15
Kumar Ashwani <kashwani@paypal.com>
2025-03-05 23:14:29 -0800

    Disable Spring Boot banner in both application.properties and main application class

--- src/main/java/com/example/mcpserver/PayPalMcpServerApplication.java
+++ src/main/java/com/example/mcpserver/PayPalMcpServerApplication.java
@@ -2,11 +2,14 @@ package com.example.mcpserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.Banner;
 
 @SpringBootApplication
 public class PayPalMcpServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PayPalMcpServerApplication.class, args);
+        SpringApplication app = new SpringApplication(PayPalMcpServerApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 } 
